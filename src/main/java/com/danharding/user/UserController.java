eb8c365265657fae1da05af35bb309ff5f9a1521
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-06-25 20:34:14 -0400

    login authentication in progress

--- src/main/java/com/danharding/user/UserController.java
+++ src/main/java/com/danharding/user/UserController.java
@@ -1,14 +1,24 @@
 package com.danharding.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 import javax.validation.Valid;
+
+import java.util.Arrays;
 import java.util.List;
 
 
 @RestController
 public class UserController {
+	private static final String SIGN_UP_URL = null;
 	@Autowired
 	UserRepository userRepository;
 	
@@ -66,5 +76,25 @@ public class UserController {
 	public Status deleteUsers() {
 		userRepository.deleteAll();
 		return Status.SUCCESS;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors()
+		.and()
+		.csrf().disable()
+		.authorizeRequests().antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+		.anyRequest().authenticated();
+		
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.applyPermitDefaultValues();
+		corsConfig.setExposedHeaders(Arrays.asList("Authorization"));
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
 	}
 }
