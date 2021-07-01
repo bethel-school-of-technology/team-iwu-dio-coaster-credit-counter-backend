b20a704cba0f10f0e733e37679835d06d5c8aa33
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-07-01 11:58:23 -0400

    removed unused import, forgot to do so in last push

--- src/main/java/com/danharding/finalproject/bucketList/BucketListController.java
+++ src/main/java/com/danharding/finalproject/bucketList/BucketListController.java
@@ -2,7 +2,7 @@ package com.danharding.finalproject.bucketList;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
