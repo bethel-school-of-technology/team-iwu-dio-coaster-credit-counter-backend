ba28257b90065abadd9d3c5ba842279eaccbfaf0
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-07-04 17:38:14 -0400

    working on putting validation and encryption on the backend

--- src/main/java/com/danharding/finalproject/user/User.java
+++ src/main/java/com/danharding/finalproject/user/User.java
@@ -7,7 +7,6 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import java.util.Objects;
 
-
 @Entity
 @Table(name = "users")
 public class User {
@@ -18,43 +17,44 @@ public class User {
 	private @NotBlank String username;
 	private @NotBlank String password;
 	private @NotBlank boolean loggedIn;
-	
+
 	public User() {
-		
+
 	}
-	
-	public User (@NotBlank String username, @NotBlank String password, @NotBlank String firstName, @NotBlank String lastName) {
+
+	public User(@NotBlank String username, @NotBlank String password, @NotBlank String firstName,
+			@NotBlank String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.username = username;
 		this.password = password;
 		this.loggedIn = false;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public boolean isLoggedIn() {
 		return loggedIn;
 	}
-	
+
 	public void setLoggedIn(boolean loggedIn) {
 		this.loggedIn = loggedIn;
 	}
@@ -82,27 +82,60 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
+	public static boolean valPassword(String password) {
+		if (password.length() > 7) {
+			if(checkPassword(password)) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			System.out.println("Password too short");
+			return false;
+		}
+	}
+
+	public static boolean checkPassword(String password) {
+		boolean hasNum = false;
+		boolean hasCap = false;
+		boolean hasLow = false;
+		char c;
+		for (int i = 0; i < password.length(); i++) {
+			c = password.charAt(i);
+			if (Character.isDigit(c)) {
+				hasNum = true;
+			}
+			if (Character.isUpperCase(c)) {
+				hasCap = true;
+			} else if (Character.isLowerCase(c)) {
+				hasLow = true;
+			}
+			if (hasNum && hasCap && hasLow) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) return true;
-		if(!(o instanceof User)) return false;
+		if (this == o)
+			return true;
+		if (!(o instanceof User))
+			return false;
 		User user = (User) o;
-		return Objects.equals(username, user.username) && Objects.equals(password, user.password); 
+		return Objects.equals(username, user.username) && Objects.equals(password, user.password);
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id, username, password, loggedIn);
 	}
-	
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", loggedIn=" + loggedIn +
-                '}';
-    }
+
+	@Override
+	public String toString() {
+		return "User{" + "id=" + id + ", username='" + username + '\'' + ", password='" + password + '\''
+				+ ", loggedIn=" + loggedIn + '}';
+	}
 }
--- src/main/java/com/danharding/finalproject/user/WebSecurityConfig.java
+++ src/main/java/com/danharding/finalproject/user/WebSecurityConfig.java
@@ -2,6 +2,10 @@ package com.danharding.finalproject.user;
 
 import java.util.Arrays;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,31 +18,81 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-    
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private static final String UNICODE_FORMAT = "UTF-8";
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		System.out.println("passwordEncoder function called");
 		return new BCryptPasswordEncoder();
 	}
 
-    @Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.cors()
-		.and()
-		.csrf().disable()
-		.authorizeRequests()
-        .antMatchers(HttpMethod.POST, "/users/register").permitAll()
-		.antMatchers(HttpMethod.POST, "/users/login").permitAll()
-        .antMatchers(HttpMethod.POST, "/coasters/credits").permitAll()
-        .antMatchers(HttpMethod.POST, "/coasters/bucketlist").permitAll()
-		.anyRequest().authenticated().and()
-		.addFilter(new JWTAuthenticationFilter(authenticationManager()))
-		.addFilter(new JWTAuthorizationFilter(authenticationManager()))
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	public static void encryptDecrypt() {
 		
+		String text = "hello";
+
+		try {
+			SecretKey key = generateKey("AES");
+			Cipher cipher;
+			cipher = Cipher.getInstance("AES");
+			byte[] encryptedData = encryptString(text, key, cipher);
+			String encryptedString = new String(encryptedData);
+			System.out.println(encryptedString);
+			String decrypted = decryptString(encryptedData, key, cipher);
+			System.out.println(decrypted);
+
+		} catch (Exception e) {
+
+		}
+	}
+
+	public static SecretKey generateKey(String encryptionType) {
+		try {
+			KeyGenerator keyGenerator = KeyGenerator.getInstance(encryptionType);
+			SecretKey myKey = keyGenerator.generateKey();
+			return myKey;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public static byte[] encryptString(String dataToEncrypt, SecretKey myKey, Cipher cipher) {
+		try {
+			byte[] text = dataToEncrypt.getBytes(UNICODE_FORMAT);
+			cipher.init(Cipher.ENCRYPT_MODE, myKey);
+			byte[] textEncrypted = cipher.doFinal(text);
+
+			return textEncrypted;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public static String decryptString(byte[] dataToDecrypt, SecretKey myKey, Cipher cipher) {
+		try {
+			cipher.init(Cipher.DECRYPT_MODE, myKey);
+			byte[] textDecrypted = cipher.doFinal(dataToDecrypt);
+			String result = new String(textDecrypted);
+
+			return result;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/users/register")
+				.permitAll().antMatchers(HttpMethod.POST, "/users/login").permitAll()
+				.antMatchers(HttpMethod.POST, "/coasters/credits").permitAll()
+				.antMatchers(HttpMethod.POST, "/coasters/bucketlist").permitAll().anyRequest().authenticated().and()
+				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+				.addFilter(new JWTAuthorizationFilter(authenticationManager())).sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
 	}
 
 	@Bean
@@ -51,4 +105,3 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		return source;
 	}
 }
-
